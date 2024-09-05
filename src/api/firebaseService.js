0b53425dd94dbb44a2371935c63842ce6d388051
Patrick <47269638+PatrykPrusko2019@users.noreply.github.com>
2024-09-06 01:00:06 +0200

    Added Firebase Service which is responsible for direct communication with Firestore

---
+++ src/api/firebaseService.js
@@ -0,0 +1,24 @@
+import { db } from '../firebaseConfig';
+import { collection, addDoc, updateDoc, deleteDoc, doc, getDocs } from 'firebase/firestore';
+
+export const fetchAppointments = async () => {
+  const snapshot = await getDocs(collection(db, 'events'));
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
+
+export const addAppointment = async (appointment) => {
+  await addDoc(collection(db, 'events'), appointment);
+};
+
+export const updateAppointment = async (id, appointment) => {
+  const appointmentRef = doc(db, 'events', id);
+  await updateDoc(appointmentRef, appointment);
+};
+
+export const deleteAppointment = async (id) => {
+  const appointmentRef = doc(db, 'events', id);
+  await deleteDoc(appointmentRef);
+};
